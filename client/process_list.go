0842c763cf9f94f679796836a9e39360273aba74
Dimas Ananda traper <traperwaze@gmail.com>
2022-09-14 16:36:47 +0700

    update process_list client

--- client/process_list.go
+++ client/process_list.go
@@ -2,18 +2,20 @@ package client
 
 import (
 	"context"
-	"io"
+	"encoding/json"
 	"log"
+
+	"github.com/9d4/dimon/process"
 )
 
-func (cli *Client) ProcessList(ctx context.Context) {
+func (cli *Client) ProcessList(ctx context.Context) ([]process.Process, error) {
 	resp, err := cli.get(ctx, "/processes", nil, nil)
+	defer ensureReaderClosed(resp)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	body, err := io.ReadAll(resp.body)
-	if err == nil {
-		log.Println(string(body))
-	}
+	var process []process.Process
+	err = json.NewDecoder(resp.body).Decode(&process)
+	return process, err
 }
