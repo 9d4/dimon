19f2caa9ed58b1f2a91d0bfe02168e818ce345f3
Dimas Ananda traper <traperwaze@gmail.com>
2022-09-25 20:06:51 +0700

    update task/store_test, remove benchmark

--- task/store_test.go
+++ task/store_test.go
@@ -2,18 +2,8 @@ package task
 
 import (
 	"testing"
-
-	"github.com/asdine/storm/v3"
 )
 
-func BenchmarkNewStore(b *testing.B) {
-	db, err := storm.Open("dimon.db")
-	if err != nil {
-		b.Fatal(err)
-	}
-
-	// for i := 0; i < b.N; i++ {
-	// 	NewStore(db)
-	// }
-	NewStore(db)
+func TestEnsuresImplementStore(t *testing.T) {
+	var _ Store = &store{}
 }
