1354f2da2737acef3d62b93cf21a177e8d218e3c
Dimas Ananda traper <traperwaze@gmail.com>
2022-09-25 20:06:08 +0700

    add task.Store interface that the task.store should implement it

--- task/store.go
+++ task/store.go
@@ -4,28 +4,34 @@ import (
 	"github.com/asdine/storm/v3"
 )
 
-type Store struct {
+type Store interface {
+	GetAll() ([]*Task, error)
+	Get(int) (*Task, error)
+	Save(*Task) error
+}
+
+type store struct {
 	db *storm.DB
 }
 
-func NewStore(db *storm.DB) *Store {
-	return &Store{
+func NewStore(db *storm.DB) *store {
+	return &store{
 		db: db,
 	}
 }
 
-func (t Store) GetAll() ([]*Task, error) {
+func (t store) GetAll() ([]*Task, error) {
 	var tasks []*Task
 	err := t.db.All(&tasks)
 
 	return tasks, err
 }
 
-func (t Store) Get(id int) (*Task, error) {
+func (t store) Get(id int) (*Task, error) {
 	var task Task
 	return &task, t.db.One("ID", id, &task)
 }
 
-func (t Store) Save(task *Task) error {
+func (t store) Save(task *Task) error {
 	return t.db.Save(task)
 }
